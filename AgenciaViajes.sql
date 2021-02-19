3b786e406c832572c465c0302f8d63c6d51f4b95
HugoAntonioPinedaElias <78966957+HugoAntonioPinedaElias@users.noreply.github.com>
2021-02-19 08:51:56 -0600

    Crear base de datos y tablas Sucursal, Vuelos
    ewe

--- AgenciaViajes.sql
+++ AgenciaViajes.sql
@@ -1,2 +1,17 @@
 create database AgenciaViajes;
 use AgenciaViajes;
+
+create table Sucrusal (
+CodigoSucursal int primary key,
+Direccion varchar(60),
+Telefono varchar(60)
+);
+
+create table Vuelos (
+NoVuelo int primary key,
+FechaVuelo SmallDateTime,
+Origen varchar(30),
+Destino varchar(30),
+PlazasTotales int,
+PlazasTurista int,
+);
